eb268e4e2aae6628b95eaa3ac9386bb7457515c8
mbazan16 <mariabazan@gmail.com>
2024-05-13 13:24:42 +0200

    Ejemplo de Thymeleaf

--- demo/src/main/java/com/example/demo/controllers/admin/BuscadorDepartamentoController.java
+++ demo/src/main/java/com/example/demo/controllers/admin/BuscadorDepartamentoController.java
@@ -5,29 +5,31 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.admin.IAdminDepartamentos;
 import com.example.demo.admin.exception.AdminException;
 import com.example.demo.data.Departamento;
 
-@RestController
-@RequestMapping("/saludo")
+@Controller
+@RequestMapping("/buscador")
 public class BuscadorDepartamentoController {
 	public static final Logger log = LoggerFactory.getLogger(BuscadorDepartamentoController.class);
 	
 	@Autowired
 	IAdminDepartamentos servicio;
 	
-	@GetMapping()
-	public List<Departamento> buscador(@RequestParam("cadena") String cadena) throws AdminException{
+	@GetMapping
+	public String buscador(Model model,@RequestParam("cadena") String cadena) throws AdminException{
 		log.info("[buscador]");
 		log.debug("[cadena:"+cadena+"]");
-		return servicio.buscador(cadena);		
+		List<Departamento> departamentos= servicio.buscador(cadena);	
+		model.addAttribute("listaDepartamentos", departamentos);
+		return "t_inicio";
 	}
 	
 
